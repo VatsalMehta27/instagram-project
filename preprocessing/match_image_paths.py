e89e77a7715b1a8e751c91ef4268700e7d745af1
Vatsal Mehta <vatsalbmehta3@gmail.com>
2023-03-23 13:15:40 -0400

    modified to get first image name from img zip file for correct csv matching

--- preprocessing/match_image_paths.py
+++ preprocessing/match_image_paths.py
@@ -1,5 +1,16 @@
 import pandas as pd
 from numpy import nan
+import tarfile
+
+def get_first_image_num(username: str) -> int:
+    folder_path = f"/img/{username}.tar.gz"
+
+    with tarfile.open(folder_path, "r:gz") as folder:
+        img_path_names = map(lambda img_path: int(img_path.split("/")[1].split(".")[0]), folder.getnames()[1:])
+
+        assert max(img_path_names) - min(img_path_names) == len(img_path_names) - 1
+    
+    return min(img_path_names)
 
 # Read in the all_posts_metadata CSV file
 all_posts_metadata = pd.read_csv("data/all_posts_metadata.csv")
@@ -21,7 +32,7 @@ for index, row in all_posts_metadata.iterrows():
     username_image_count = username_image_counter.get(username)
 
     if username_image_count is None:
-        username_image_counter[username] = 0
+        username_image_counter[username] = get_first_image_num(username)
 
     if row["type"] == "Photo":
         image_path.append(f"{username_image_counter[username]}.jpg")
